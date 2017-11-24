ff524583571136919d2e3127b55cf64ac345f7cb
count685 <no@mail.com>
2017-11-24 23:46:27 +0300

    Simple bot v1

---
+++ bot.py
@@ -0,0 +1,35 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def start_bot(bot, update):
+    print(update)
+    mytext = """Привет {}!
+Я простой бот и понимаю только команду /start
+    """.format(update.message.chat.first_name)
+    update.message.reply_text(mytext)
+
+def chat(bot, update):
+    text = update.message.text
+    logging.info(text)
+    update.message.reply_text(text)
+
+def main():
+    updrt = Updater(settings.TELEGRAM_API_KEY)
+
+    updrt.dispatcher.add_handler(CommandHandler('start', start_bot))
+    updrt.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
+    updrt.start_polling()
+    updrt.idle()
+
+if __name__ == '__main__':
+    logging.info('Bot started')
+    main()
+
